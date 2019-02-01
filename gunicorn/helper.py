3dbb9aec1ecb5286813cdf6b4a335a84487250e7
Your Name <you@example.com>
2019-02-01 11:10:30 +0800

    修复gunicorn的启动bu gbug

--- gunicorn/helper.py
+++ gunicorn/helper.py
@@ -5,22 +5,23 @@ import os
 def run(command, strip=True):
     lines = os.popen(command).readlines()
     if not strip:
-        return "".join(r)
+        return "".join(lines)
     lines = [line.strip() for line in lines]
     return "\n".join(lines)
 
 def gunicorn_pids(app):
     try :
-        lines = run("ps aux|grep gunicorn|grep '%s'|awk '{print $2}'" % app, strip=False)
-        pids = [int(line.strip()) for line in lines]
+        r = run("ps aux|grep gunicorn|grep '%s'|grep -v 'ps aux'|awk '{print $2}'" % app)
+        lines = r.split("\n")
+        pids = [int(line) for line in lines]
+        return pids
     except:
         return []
 
-def gunicorn_run(app, wsgi, port, worker):
-    cmd_template = "gunicorn -D -w {worker} -b 0.0.0.0:{port} {app}:{wsgi}"
-    command = cmd_template.format(worker=worker, port=port, app=app, wsgi=wsgi)
+def gunicorn_run(workdir, module_wsgi, port, worker):
+    cmd_template = "gunicorn -D -w {worker} -b 0.0.0.0:{port} {module_wsgi}"
+    command = cmd_template.format(worker=worker, port=port, module_wsgi=module_wsgi)
     temp_cwd = os.getcwd()
-    work_dir = os.path.dirname(APP)
-    os.chdir(work_dir)
+    os.chdir(workdir)
     run(command)
     os.chdir(temp_cwd)
